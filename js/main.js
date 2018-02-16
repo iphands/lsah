2fe7ab68f6315ad7bd11b41d4b43f70feaa03b1a
Ian Page Hands <iphands@gmail.com>
2018-02-15 21:03:56 -0500

    Angularize the member list

--- js/main.js
+++ js/main.js
@@ -1,32 +1,24 @@
 /*global jQuery, window, angular*/
 "use strict";
-(function ($) {
-    var items = [];
-    var number = 1;
-    window.members.forEach(function (item) {
-        var country = item.country ? item.country.toLowerCase() : 'us';
-        items.push(
-            "<tr>" +
-                "<td>" + (item.numberHack || number) + "</td>" +
-                "<td>" +
-                "<span data-toggle=\"tooltip\" data-placement=\"top\" title=\"" + window.countries[country] + "\" class=\"flag-icon flag-icon-" + country + "\"></span>" +
-                "</td>" +
-                "<td>" + item.name + "</td>" +
-                "<td>" + item.years.join(', ') + "</td>" +
-                "<td>" + item.times.join(', ') + "</td>" +
-                "<td class=\"d-none d-sm-block\">" + (item.notes ? item.notes : '') + "</td>" +
-                "</tr>"
-        );
-
-        if (!item.numberHack) {
-            number += 1;
-        }
-    });
-    $('#membersTable tbody').append(items.join());
-    $('[data-toggle="tooltip"]').tooltip();
-}(jQuery));
-
 (function () {
     var app = angular.module('LSAH', []);
     app.controller('EmailAppCtrl', function EmailAppCtrl() {});
+
+    app.controller('TableListCrtl', function TableListCrtl($scope) {
+        var number = 1;
+        window.members.forEach(function (item) {
+            var country = item.country ? item.country.toLowerCase() : 'us';
+
+            item.count = item.numberHack || number;
+            item.country = country;
+            item.countryFull = window.countries[country];
+            item.years = item.years.join(', ');
+            item.times = item.times.join(', ');
+            if (!item.numberHack) {
+                number += 1;
+            }
+        });
+
+        $scope.members = window.members;
+    });
 }());

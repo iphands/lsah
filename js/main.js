dc07e6e81b28a1b6a9f2bf9b4d4f9fd64a9dd086
Ian Page Hands <iphands@gmail.com>
2023-09-08 08:28:59 -0700

    format only

--- js/main.js
+++ js/main.js
@@ -1,107 +1,107 @@
 /* global document, window, angular */
 "use strict";
 (function (jq, lodash) {
-    const app = angular.module("LSAH", []);
+  const app = angular.module("LSAH", []);
 
-    app.controller("EmailAppCtrl", function EmailAppCtrl () {});
+  app.controller("EmailAppCtrl", function EmailAppCtrl () {});
 
-    app.controller("TableListCrtl", function TableListCrtl ($scope) {
-        let once = true;
+  app.controller("TableListCrtl", function TableListCrtl ($scope) {
+    let once = true;
 
-        function genYears (members) {
-            let years = [];
-            members.forEach(function (m) {
-                years = lodash.concat(years, m.years);
-            });
+    function genYears (members) {
+      let years = [];
+      members.forEach(function (m) {
+        years = lodash.concat(years, m.years);
+      });
 
-            // uniq | sort
-            years = [...new Set(years)].sort();
+      // uniq | sort
+      years = [...new Set(years)].sort();
 
-            return years;
+      return years;
+    }
+
+    function init () {
+      let number = 1;
+      const countries = {};
+
+      $scope.years = genYears(window.members);
+
+      window.members.forEach(function (item) {
+        const country = item.country ? item.country.toLowerCase() : "us";
+
+        item.count = item.numberHack || number;
+        item.country = country;
+        item.countryFull = window.countries[country];
+        item.years = item.years.join(", ");
+        item.times = item.times.join(", ");
+
+        if (!item.numberHack) {
+          number += 1;
         }
 
-        function init () {
-            let number = 1;
-            const countries = {};
-
-            $scope.years = genYears(window.members);
-
-            window.members.forEach(function (item) {
-                const country = item.country ? item.country.toLowerCase() : "us";
-
-                item.count = item.numberHack || number;
-                item.country = country;
-                item.countryFull = window.countries[country];
-                item.years = item.years.join(", ");
-                item.times = item.times.join(", ");
-
-                if (!item.numberHack) {
-                    number += 1;
-                }
-
-                if (!countries.hasOwnProperty(country)) {
-                    countries[country] = {
-                        code: country,
-                        label: window.countries[country],
-                        count: 1
-                    };
-                } else {
-                    countries[country].count += 1;
-                }
-            });
-
-            $scope.members = window.members;
-            $scope.countries = lodash.sortBy(lodash.values(countries), "count").reverse();
-            $scope.countriesTotal = lodash.reduce($scope.countries, function (sum, n) {
-                return sum + n.count;
-            }, 0);
+        if (!countries.hasOwnProperty(country)) {
+          countries[country] = {
+            code: country,
+            label: window.countries[country],
+            count: 1
+          };
+        } else {
+          countries[country].count += 1;
         }
+      });
 
-        function prepopulateMembers () {
-            const tmp = [];
-            for (let i = 0; i < window.members.length + 50; i += 1) {
-                tmp.push({ name: "Loading..." });
-            }
-            $scope.members = tmp;
-        }
+      $scope.members = window.members;
+      $scope.countries = lodash.sortBy(lodash.values(countries), "count").reverse();
+      $scope.countriesTotal = lodash.reduce($scope.countries, function (sum, n) {
+        return sum + n.count;
+      }, 0);
+    }
 
-        prepopulateMembers();
+    function prepopulateMembers () {
+      const tmp = [];
+      for (let i = 0; i < window.members.length + 50; i += 1) {
+        tmp.push({ name: "Loading..." });
+      }
+      $scope.members = tmp;
+    }
 
-        document.addEventListener("scroll", function () {
-            if (once && jq("#members").is(":in-viewport")) {
-                once = false;
-                init();
-                $scope.$digest();
-            }
-        });
+    prepopulateMembers();
 
-      window.setTimeout(() => {
-	if (once && jq("#members").is(":in-viewport")) {
-          once = false;
-          init();
-          $scope.$digest();
-        }
-      }, 1000);
+    document.addEventListener("scroll", function () {
+      if (once && jq("#members").is(":in-viewport")) {
+        once = false;
+        init();
+        $scope.$digest();
+      }
     });
+
+    window.setTimeout(() => {
+      if (once && jq("#members").is(":in-viewport")) {
+        once = false;
+        init();
+        $scope.$digest();
+      }
+    }, 1000);
+  });
 }(window.jQuery, window._));
 
 (function (jq) {
-    const total = document.querySelectorAll("img[data-src]").length;
-    let run = true;
-    let done = 0;
+  const total = document.querySelectorAll("img[data-src]").length;
+  let run = true;
+  let done = 0;
 
-    document.addEventListener("scroll", function () {
-        if (run) {
-            const images = jq("img[data-src]:in-viewport");
-            if (images.length) {
-                images.attr("src", function (i) {
-                    const src = images[i].getAttribute("data-src");
-                    images[i].removeAttribute("data-src");
-                    done += 1;
-                    if (total === done) { run = false; }
-                    return src;
-                });
-            }
-        }
-    });
+  document.addEventListener("scroll", function () {
+    if (run) {
+      const images = jq("img[data-src]:in-viewport");
+      if (images.length) {
+        images.attr("src", function (i) {
+          const src = images[i].getAttribute("data-src");
+          images[i].removeAttribute("data-src");
+          done += 1;
+          if (total === done) { run = false; }
+          return src;
+        });
+      }
+    }
+  });
 }(window.jQuery));
